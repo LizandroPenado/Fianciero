a32cb54aeed6918ba90adb19b0ae8fb04917af1a
Aragi group <aragidg@gmail.com>
2021-10-01 08:38:21 -0600

    Configuracion inicial del frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Cuenta from "./components/cuenta/Cuenta";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main /* className={classes.content} */>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/cuenta" component={Cuenta} />
+        </Switch>
+      </Router>
+    </main>
   );
 }
 

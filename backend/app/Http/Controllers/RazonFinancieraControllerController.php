2b4835cebc6d9a02c56e473cc957db0b6dd5e035
Eduardo Orellana <og17006@ues.edu.sv>
2021-11-12 01:30:44 -0600

    Crear migraciones de Ratios y su respectivo model y controller

---
+++ backend/app/Http/Controllers/RazonFinancieraControllerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RazonFinancieraController;
+use Illuminate\Http\Request;
+
+class RazonFinancieraControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RazonFinancieraController  $razonFinancieraController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RazonFinancieraController $razonFinancieraController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RazonFinancieraController  $razonFinancieraController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RazonFinancieraController $razonFinancieraController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RazonFinancieraController  $razonFinancieraController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RazonFinancieraController $razonFinancieraController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RazonFinancieraController  $razonFinancieraController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RazonFinancieraController $razonFinancieraController)
+    {
+        //
+    }
+}
---
+++ frontend/src/components/ratios/InformeRatios.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Col, Form, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import ReactToPrint from "react-to-print";
+import Opciones from "./Opciones";
+import TablaAnalisis from "./TablaAnalisis";
+import axios from "axios";
+import Swal from "sweetalert2";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+
+export const InformeRatios = (props) => {
+    return{
+
+    }
+}

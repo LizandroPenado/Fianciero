fd635eb9fbd6daac31ff562c6bd30f00a07ecb7f
Eduardo Orellana <og17006@ues.edu.sv>
2021-11-14 11:05:57 -0600

    diseño basico para el home

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,28 +1,22 @@
 import React, { Component } from "react";
-import { Col, Container, Row } from "react-bootstrap";
 import "./Home.css"
 
 class Home extends Component {
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col sm={8} className="align-self-center" align="center">
-            <h2>Bienvenido *Nombre del usuario*</h2>
-          </Col>
-          <Col sm={4} className="align-self-center">
-            <h3 >*Nombre del sistema*</h3>
-            <p >
-              Sistema informatico, para la implementación de análisis
-              financieros.
+      <>
+        <div className="home-container">
+          <div className="home-title">
+            <h1>Bienvenido</h1>
+          </div>
+          <div className="home-info">
+            <h3>Sistema Financiero - G02</h3>
+            <p>
+              Este sistema es una <b>implementación</b> de los diferentes informes financieron que una empresa puede efectuar para determinar su progreso anualmente.
             </p>
-            <p>Ingresa informacion: permite...</p>
-            <p>Análisis horizontal: permite...</p>
-            <p>Análisis Vertical: permite...</p>
-            <p>Graficar cuentas: permite...</p>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </div>
+      </>
     );
   }
 }
